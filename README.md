fdf1a1e51d4b03acdb1cf5f0f6599b4bd4c06355
Raoni Fantinel <raonifantinel@gmail.com>
2022-01-26 11:59:17 -0300

    add comment and fix remove method

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A simple command line budget program for object orientation praticing.
 
 ## How the program works?
 
-The program asks for a budget year, witch has to be at least the current year. Then, the user needs to provide budget categories and the available cash for those categories. After that, the user can choose to update his budget (deposit, withdraw, transfer, add or remove categories). When finshed, an output text file containing the budget info is generated on the current directory.
+The program asks for a budget year, witch has to be at least the current year. Then, the user needs to provide budget categories and the available cash for those categories. After that, the user can choose to update his budget (deposit, withdraw, transfer and remove categories). When finshed, an output text file containing the budget info is generated on the current directory.
 
 - To start the program, type "python budget.py" or "python3 budget.py" on terminal.
 - To quit the program, just type "exit".
--- budget.py
+++ budget.py
@@ -6,50 +6,67 @@ budget = []
 MAX_LENGTH = 100
 
 def main():
-
+    
+    # Get budget year
     year = get_year()
+
+    # Add budget category and available cash
     add_category()
 
     q1 = input("Would you like to update your budget? ").lower()
     if q1 == "exit":
         exit_program()
+
+    # Update budget
     while q1 != "0":
+
         total = 0
+        # Print current budget
         print(f"Budget for {year}")
         for d in budget:
             for key in d:
                 print(f"{key} = {d[key].cash} ")
                 total += d[key].cash
         print(f"total = {total}")
+
+        # Print update options
         with open("options.txt", "r") as f:
             get_options = f.read()
             print(get_options)
         q2 = input("Select one of the options above: ").lower()
         if q2 == "0" or q2 == "exit":
             exit_program()
+        # Deposit
         elif q2 == "1":
             index = find_category("Deposit into: ")
             value = get_cash_value("Value: ")
             budget[index[0]][index[1]].deposit(value)
+        # Withdraw
         elif q2 == "2":
             index = find_category("Withdraw from: ")
             value = get_cash_value("Value: ")
             budget[index[0]][index[1]].withdraw(value)
+        # Transfer
         elif q2 == "3":
             origin = find_category("Origin: ")
             value = get_cash_value("Value: ")
             destination = find_category("Destination: ")
             budget[origin[0]][origin[1]].withdraw(value)
             budget[destination[0]][destination[1]].deposit(value)
+        # Add category
         elif q2 == "4":
             add_category()
+        # Remove category
         elif q2 == "5":
             remove_category()
+        # Exit program
         elif q2 == "exit":
             exit_program()
         q1 = input("Would you like to update your budget? ").lower()
 
+
     total = 0
+    # Write output file
     with open("out.txt", "w", encoding="utf-8") as f:
         f.write(f"Budget for {year} \n")
         for d in budget:
@@ -99,7 +116,9 @@ def remove_category():
         if usr_input == category:
             index = categories.index(usr_input)
             categories.remove(usr_input)
-            budget[index][usr_input].pop()
+            budget[index].pop(usr_input)
+            if len(categories) == 0:
+                sys.exit("All categories removed.")
 
 def find_category(m):
     while True:
