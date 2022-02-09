2d6ce73412fa05e676864a3b9ab2764f08510c04
Raoni Fantinel <raonifantinel@gmail.com>
2022-02-09 19:39:47 -0300

    update budget.py and config.py

--- budget.py
+++ budget.py
@@ -1,136 +1,80 @@
 import sys
 from config import *
 
-categories = []
-budget = []
-MAX_LENGTH = 100
-
 def main():
     
     # Get budget year
     year = get_year()
 
     # Add budget category and available cash
-    add_category()
+    budget = add_categories()
 
-    q1 = input("Would you like to update your budget? ").lower()
+    q = input("Would you like to update your budget? ").lower()
     # Update budget
-    while q1 != "0":
-        
-        if q1 == "exit":
+    while q != "0":
+
+        if q == "exit":
             exit_program()
 
         total = 0
         # Print current budget
         print(f"Budget for {year}")
-        for d in budget:
-            for key in d:
-                print(f"{key} = {d[key].cash} ")
-                total += d[key].cash
-        print(f"total = {total}")
+        for item in budget:
+            print(f"{item.category} = {item.cash}")
+            total += item.cash
+        print(f"Total = {total}")
 
         # Print update options
         with open("options.txt", "r") as f:
             get_options = f.read()
             print(get_options)
-        q2 = input("Select one of the options above: ").lower()
-        if q2 == "0" or q2 == "exit":
-            exit_program()
-        # Deposit
-        elif q2 == "1":
-            index = find_category("Deposit into: ")
-            value = get_cash_value("Value: ")
-            budget[index[0]][index[1]].deposit(value)
-        # Withdraw
-        elif q2 == "2":
-            index = find_category("Withdraw from: ")
-            value = get_cash_value("Value: ")
-            budget[index[0]][index[1]].withdraw(value)
-        # Transfer
-        elif q2 == "3":
-            origin = find_category("Origin: ")
-            value = get_cash_value("Value: ")
-            destination = find_category("Destination: ")
-            budget[origin[0]][origin[1]].withdraw(value)
-            budget[destination[0]][destination[1]].deposit(value)
-        # Add category
-        elif q2 == "4":
-            add_category()
-        # Remove category
-        elif q2 == "5":
-            remove_category()
-        # Exit program
-        elif q2 == "exit":
-            exit_program()
-        q1 = input("Would you like to update your budget? ").lower()
 
+        q = input("Select one of the options above: ").lower()
+        if q == "exit":
+            continue
+        
+        match q:
+            case "0":
+                break
+            case "1":
+                # Deposit
+                c = find(budget, "Deposit into: ")
+                v =  get_cash_value("Value: ")
+                budget[c].deposit(v)
+            case "2":
+                # Withdraw
+                c = find(budget, "Withdraw from: ")
+                v =  get_cash_value("Value: ")
+                budget[c].withdraw(v)
+            case "3":
+                # Transfer
+                c = find(budget, "Origin: ")
+                t = c
+                v = get_cash_value("Value: ")
+                c = find(budget, "Destination: ")
+                budget[t].withdraw(v)
+                budget[c].deposit(v)
+            case "4":
+                # Add category
+                add_categories(budget)
+            case "5":
+                remove_category(budget)
 
+        q = input("Would you like to update your budget? ").lower()
+
+    
     total = 0
     # Write output file
     with open("out.txt", "w", encoding="utf-8") as f:
-        f.write(f"Budget for {year} \n")
-        for d in budget:
-            for key in d:
-                f.write(f"{key} = {d[key].cash} \n")
-                total += d[key].cash
-        f.write(f"total = {total} \n")
-
-    print("Budget completed!")
-
-def add_category():
-
-    count = len(categories)
-    while count < MAX_LENGTH:
-        usr_input = input("Add category (type 0 to stop): ").lower()
-        if usr_input == "0" and count == 0:
-            sys.exit("No categories were added.")
-        elif usr_input == "0":
-            break
-        elif usr_input == "exit":
-            exit_program()
-        else:
-            if count != 0:
-                for category in categories:
-                    if usr_input == category:
-                        print("Category already exists.")
-                        break
-                    if categories.index(category) == count - 1:
-                        categories.append(usr_input)
-                        count += 1
-                        break
-            else:
-                categories.append(usr_input)
-                count += 1
-        if count == MAX_LENGTH:
-            print("Limit reached.")
+        f.write(f"Budget for {year}\n")
+        for item in budget:
+            f.write(f"{item.category} = {item.cash}\n")
+            total += item.cash
+        f.write(f"Total = {total}\n")
     
-    for category in categories:
-        cash = get_cash_value(f"Available cash for {category}: ")
-        budget.append({category : Budget(cash)})
+    return print("Budget completed!")
 
-def remove_category():
-    usr_input = input("Remove category: ").lower()
-    if usr_input == "exit":
-        exit_program()
-    for category in categories:
-        if usr_input == category:
-            index = categories.index(usr_input)
-            categories.remove(usr_input)
-            budget[index].pop(usr_input)
-            if len(categories) == 0:
-                sys.exit("All categories removed.")
 
-def find_category(m):
-    while True:
-        usr_input = input(m)
-        if usr_input == "exit":
-            exit_program()
-        for category in categories:
-            if usr_input == category:
-                return [categories.index(category), usr_input]
-            if categories.index(category) == len(categories) - 1:
-                print("Category not found.")
-                break
 
 if __name__ == "__main__":
     main()
--- config.py
+++ config.py
@@ -2,11 +2,12 @@ from datetime import date
 import sys
 
 class Budget:
-    def __init__(self, cash):
+    def __init__(self, category, cash):
         self.cash = cash
+        self.category = category
 
     def deposit(self, value):
-        self.cash += value
+            self.cash += value
 
     def withdraw(self, value):
         if value <= self.cash:
@@ -14,6 +15,71 @@ class Budget:
         else:
             return print("Not enough cash.")
 
+# Create budget object and add to list
+def add_categories(budget=[], n=100):
+
+    c = []
+    if budget:
+        for item in budget:
+            c.append(item.category)
+        
+    count = len(c)
+    index = 0    
+    while count < n:
+        q = input("Add category (type 0 to stop): ").lower()
+        if q == "0" and count == 0:
+            sys.exit("No categories were added.")
+        elif q == "0":
+            break
+        elif q == "exit":
+            exit_program()
+        else:
+            if q in c:
+                print("Category already added.")
+                continue
+            else:
+                c.append(q)
+                count += 1
+                index += 1
+
+        if count == n:
+            print("Limit reached.")
+
+    for i in range(count - index, count):
+        cash = get_cash_value(f"Available cash for {c[i]}: ")
+        budget.append(Budget(c[i], cash))
+
+    return budget
+
+# Remove objects from budget list
+def remove_category(budget):
+    
+    count = len(budget)
+    while True:
+        r = find(budget, "Remove category (type 0 to stop): ")
+        if r != None:
+            budget.remove(budget[r])
+            count -= 1
+            if count == 0:
+                sys.exit("All categories removed.")
+       
+
+# Find categories
+def find(list, m):
+    while True:
+        q = input(m)
+        if q == "0":
+            return None
+        elif q == "exit":
+            exit_program()
+
+        for i in range(0, len(list)):
+            if list[i].category == q:
+                return i
+            if i == len(list) - 1:
+                print("Category not found.")
+
+# Get positive float
 def get_cash_value(m):
     while True:
         cash = input(m)
@@ -32,6 +98,7 @@ def get_cash_value(m):
                 continue
     return cash
 
+# Get valid year
 def get_year():
     while True:
         year = input("Budget year: ")
